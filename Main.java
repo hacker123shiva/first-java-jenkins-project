64aa7106bd8c4c3784dc67e39dc8d9b417703376
Shiva srivastava <105240067+hacker123shiva@users.noreply.github.com>
2024-03-17 00:04:08 +0530

    this is my first jenkins project

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java Jenkins world");
+        System.out.println("I am learning Jenkins with Java and Git");
+        System.out.println(new Date().getTime());
+
+    }
+}
